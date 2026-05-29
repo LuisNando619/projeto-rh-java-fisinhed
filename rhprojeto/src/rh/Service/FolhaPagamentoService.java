63a2d9c83f83a2df82dce1414edb35052cb0a287
danilomedeiros <danilomedeiros1006@gmail.com>
2026-05-28 21:35:53 -0300

    Add files via upload

---
+++ rhprojeto/src/rh/Service/FolhaPagamentoService.java
@@ -0,0 +1,31 @@
+package rh.Service;
+
+import rh.model.FolhaPagamento;
+import rh.model.Funcionario;
+
+public class FolhaPagamentoService {
+
+    public FolhaPagamento gerarFolha(int id, Funcionario funcionario) {
+
+        double salarioBase = funcionario.getSalarioBase();
+
+        double descontoFaltas =
+                (salarioBase / 30) * funcionario.getFaltas();
+
+        double valorHora = salarioBase / 220;
+
+        double valorHorasExtras =
+                funcionario.getHorasExtras() * (valorHora * 1.5);
+
+        double salarioFinal =
+                salarioBase - descontoFaltas + valorHorasExtras;
+
+        return new FolhaPagamento(
+                id,
+                funcionario,
+                descontoFaltas,
+                valorHorasExtras,
+                salarioFinal
+        );
+    }
+}
