cf4bb4260bdf21fbc52cec9560dfbb9432540941
danilomedeiros <danilomedeiros1006@gmail.com>
2026-05-28 21:38:22 -0300

    Add files via upload

---
+++ rhprojeto/src/rh/service/AutenticacaoService.java
@@ -0,0 +1,26 @@
+package rh.service;
+
+import rh.model.Usuario;
+import rh.repository.UsuarioRepository;
+
+public class AutenticacaoService {
+
+    private UsuarioRepository repository;
+
+    public AutenticacaoService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public Usuario login(String login, String senha) {
+
+        for (Usuario usuario : repository.listar()) {
+            if (usuario.getLogin().equals(login)
+                    && usuario.getSenha().equals(senha)) {
+                return usuario;
+            }
+        }
+
+        return null;
+    }
+}
+
