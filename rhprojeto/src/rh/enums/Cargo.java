f8d678c40866707d683b49ee93f4b320fbbf305e
biancanery <biancanery2025@gmail.com>
2026-05-31 18:43:08 -0300

    Refactor Cargo enum to add salary base
    Updated Cargo enum to include base salary for each role.

--- rhprojeto/src/rh/enums/Cargo.java
+++ rhprojeto/src/rh/enums/Cargo.java
@@ -1,9 +1,20 @@
-package rh.enums;
+package rh.model;
 
-    public enum Cargo {
-        DESENVOLVEDOR,
-        GERENTE,
-        RH
+public enum Cargo {
+
+    DESENVOLVEDOR(5000),
+    GERENTE(8000),
+    RH(4000);
+
+    private final double salarioBase;
+
+    Cargo(double salarioBase) {
+        this.salarioBase = salarioBase;
     }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+}
 
 
